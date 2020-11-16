5353664a6ecc49a4383bc0583afff8bd4d125a18
Srdjan Strbanovic <srdjan@timok.com>
2020-11-15 22:11:11 -0500

    bump deps

--- next.config.js
+++ next.config.js
@@ -1,28 +1,10 @@
-module.exports = {
-  experimental: {
-    modern: true,
-    polyfillsOptimization: true
-  },
-  webpack(config) {
-    const splitChunks = config.optimization && config.optimization.splitChunks
-    if (splitChunks) {
-      const cacheGroups = splitChunks.cacheGroups;
-      const preactModules = /[\\/]node_modules[\\/](preact|preact-render-to-string|preact-context-provider)[\\/]/;
-      if (cacheGroups.framework) {
-        cacheGroups.preact = Object.assign({}, cacheGroups.framework, {
-          test: preactModules
-        });
-        cacheGroups.commons.name = 'framework';
-      }
-      else {
-        cacheGroups.preact = {
-          name: 'commons',
-          chunks: 'all',
-          test: preactModules
-        };
-      }
-    }
+const withPreact = require('next-plugin-preact')
 
-    return config;
+module.exports = withPreact({
+  experimental: {
+    modern: true
+  },
+  images: {
+    domains: ['firebasestorage.googleapis.com']
   }
-};
+})
